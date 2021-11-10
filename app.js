6bbe0dc3397329e2bbdcfbcbab384d198f32ea01
najwanan <najwanan@hotmail.com>
2021-11-10 15:13:33 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+//Constructor for Book objects 
+//should contain: title, author, pages, read/not read status
+// put a function into the constructor that can report the book:
+//theHobbit.info() "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+
+function Book(title, author, pages, readStatus){
+    this.title = title; 
+    this.author = author;
+    this.pages = pages; 
+    this.readStatus = read;
+
+    function statement(){
+        console.log("the" + Book.title + "by" + Book.author + Book.pages + Book.ReadStatus) 
+
+    }
+}
+
